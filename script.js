08285f12dc7015fcc05bc2f206342478d50ebdc2
Ifeson jonas <156526177+ifeson-jonas@users.noreply.github.com>
2025-02-22 21:15:38 -0300

    Criada landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const pedidoButton = document.querySelector('#contato a');
+    pedidoButton.addEventListener('click', function() {
+        alert('Obrigado por escolher o Satí de Batatas Recheadas! Seu pedido será preparado com carinho.');
+    });
+});
